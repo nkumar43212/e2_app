793663d85903e8df9a896028039970116cb10829
Abbas Sakarwala <sakarwala77@gmail.com>
2016-09-19 00:44:48 -0700

    Shared code changes

---
+++ src/shared/config.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2016, Juniper Networks, Inc.
+# All rights reserved.
+#
+# Author (Abbas Sakarwala - abbas@juniper.net)
+#
+# This module defines the E2 configuration object
+#
+
+# Libraries/Modules
+from constants import E2Constants as e2consts
+
+class E2Config(object):
+    def __init__(self):
+        # Internal config variables
+        self.pidfile = e2consts.PIDFILE
+        self.version = e2consts.VERSION
+
+        # E2 mode
+        self.mode = e2consts.MODE_ADJ_API
+
+        # The interface that E2 listens to web server (rest APIs)
+        self.host = e2consts.HOST
+
+        # The port that E2 listens on to web server (rest APIs).
+        self.http_port = e2consts.HTTP_PORT
+
+        '''
+        LOG files
+        '''
+        # TODO --- define in e2consts
+        self.log_file_path = '/tmp'             # Log file path
+        self.log_file_name = 'e2_app.log'       # Log file name
+        self.log_level = 'INFO'                 # Log level
+        self.log_file_num_backup = 10           # Number of backup log files
+        self.log_file_max = 5242880             # Maximum size to which a log file can grow = 5 MB
+
+    def __str__(self):
+        str = ""
+        str += ("pidfile=%s\n" % self.pidfile)
+        str += ("version=%s\n" % self.version)
+        str += ("mode=%s\n" % self.mode)
+        str += ("host=%s\n" % self.host)
+        str += ("http_port=%s\n" % self.http_port)
+        str += ("log_file_path=%s\n" % self.log_file_path)
+        str += ("log_file_name=%s\n" % self.log_file_name)
+        str += ("log_level=%s\n" % self.log_level)
+        str += ("log_file_num_backup=%s\n" % self.log_file_num_backup)
+        str += ("log_file_max=%s\n" % self.log_file_max)
+
+        return str
+
+
+if __name__ == "__main__":
+    import os
+    from log import Logger
+
+    # Logging
+    _LOG = Logger("E2ConfigTest", __name__, "debug")
+
+    # Set the logger handler - log, max, #files, format, level
+    filename = os.path.basename(__file__)
+    filename = filename.split(".")
+    filename = filename[0]
+    _LOG.set_handler("/tmp/" + filename + ".log", 65535, 2, "")
+    _LOG.set_level("DEBUG")
+
+    # Initialize E2 Config
+    e2config = E2Config()
+    _LOG.debug("%s"%e2config)
+
+    # Test 1 - Assign E2 mode value in E2 Config
+    e2config.mode = e2consts.MODE_ADJ_DISCOVER
+    if e2config.mode in e2consts.MODE:
+        _LOG.debug('E2 mode check succeeded ')
+    else:
+        _LOG.debug('E2 mode check failed ')
+
+    # Print after E2 mode assignment
+    _LOG.debug("%s"%e2config)
---
+++ src/shared/constants.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2016, Juniper Networks, Inc.
+# All rights reserved.
+#
+# Author (Abbas Sakarwala - abbas@juniper.net)
+#
+# This module defines the E2 constants used
+#
+
+# Libraries/Modules
+import struct
+
+class ROClass(type):
+    def __setattr__(self, name, value):
+        raise ValueError("Cannot modify %s"%name)
+
+class E2Constants(object):
+
+    """
+    This class exists only so that we can define all the E2 constants 
+    in one place.
+
+    It is not meant to be instantiated and will raise an exception if
+    an attempt is made to do so.
+    """
+    # Make sure nobody is able to modify a constant
+    __metaclass__ = ROClass
+
+    PIDFILE = '/var/run/e2_app.pid'
+    VERSION = 1.0
+
+    # E2_App IP and Port
+    HOST = '0.0.0.0'
+    HTTP_PORT = 10001
+
+    # E2 mode
+    MODE_ADJ_API = 'Adj_Api'
+    MODE_ADJ_DISCOVER = 'Adj_Discover'
+    MODE = set([MODE_ADJ_API, MODE_ADJ_DISCOVER])
+
+    # Error codes
+    SUCCESS = 0
+    FAIL = 1
+
+    # Make sure that this cannot be instantiated
+    def __new__(cls, *args, **kwargs):
+        raise TypeError("Cannot be instantiated")
+
+
+# Main function
+if __name__ == "__main__":
+    # print E2Constants.__dict__
+
+    print E2Constants.HOST
+    print E2Constants.HTTP_PORT
+    print E2Constants.MODE
+    print E2Constants.MODE_ADJ_API
+    print E2Constants.MODE_ADJ_DISCOVER
+    print E2Constants.VERSION
+    print E2Constants.SUCCESS
+    print E2Constants.FAIL
+    print E2Constants.PIDFILE
